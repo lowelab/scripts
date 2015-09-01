2c58b3756dd7426b995affb9d8f544ae8dab0d0b
lowelab <josephine.gerlach@adelaide.edu.au>
2015-09-01 11:55:48 +0930

    improved "Running structure.sh"

--- RunningStructure_CmdLine.sh
+++ RunningStructure_CmdLine.sh
@@ -4,21 +4,22 @@
 #if structure, param files and input file are in the working directory
 #just fill in the following 3 variables (and make sure the param files are completed
 
-NAME=335wolves_26msats	#supply file name suffix
-MAXK=3					#supply max number of K value to test (K = 1-? )
-NREP=3					#supply number of reps for each Kvalue test
+NAME=Testdata1			#supply file name suffix
+MAXK=4					#supply max number of K value to test (K = 1-? )
+NREP=5					#supply number of reps for each Kvalue test
 
 
 COUNTER=1 
 while [  $COUNTER -le $MAXK  ]; do 
 	REP=1
 	while [ $REP -le $NREP ]; do
-		./structure -m mainparams -e extraparams -K $COUNTER -o "$NAME"_K"$COUNTER"_rep"$REP".txt
+		nohup ./structure -m mainparams -K $COUNTER -o ./output/"$NAME"_K"$COUNTER"_rep"$REP" 2> /dev/null < /dev/null &
 		REP=$((REP+1))
 		done
 	let COUNTER=COUNTER+1
 done	
 	
+#  -e extraparams
 
 #Running STRUCTURE
 
