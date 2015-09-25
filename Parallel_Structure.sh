70f85cd4295fd60b4e14cbb4a8387b75cc94326a
lowelab <josephine.gerlach@adelaide.edu.au>
2015-09-25 09:43:10 +0930

    improve parallel_structure.sh

--- Parallel_Structure.sh
+++ Parallel_Structure.sh
@@ -9,26 +9,28 @@ NAME=Testdata1			#supply file name suffix
 MAXK=5					#supply max number of K value to test (K = 1-? )
 NREP=10					#supply number of reps for each Kvalue test
 
-
-
-
-mkdir results
-rm commands.txt
-rannumber=$[RANDOM % 99999]
+if [ ! -d ./results ]
+then mkdir results
+fi
+if [ -f ./commands.txt ]
+then rm commands.txt
+fi
 
 COUNTER=1 
 while [  $COUNTER -le $MAXK  ]; do 
 	REP=1
 	while [ $REP -le $NREP ]; do
-#	  REP=$(printf "%02d" $REP)
-#	  COUNTER=$(printf "%02d" $COUNTER)
 		echo "nohup ./structure -D $[RANDOM % 999999999] -K $COUNTER -o ./results/"$NAME"_K"$COUNTER"_rep"$REP" 2> ./nohup.out < /dev/null " >> commands.txt
 		REP=$((REP+1))
 		done
 	let COUNTER=COUNTER+1
 done	
-nohup parallel < commands.txt 
-#disown -h -ar
-
-echo "Enter 'ps' to see Structure processes running"	
+nohup parallel < commands.txt &
+sleep 2
+echo "*"
+echo "*"
+echo "Enter 'ps' to see Structure processes running"
+echo "*"
+echo "*"
+	
 #  -e extraparams -m mainparams
