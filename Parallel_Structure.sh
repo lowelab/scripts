53e256d0581ca49bdde850c19c0f6478c89df791
lowelab <josephine.gerlach@adelaide.edu.au>
2015-09-28 10:09:10 +0930

    add minK to Parallel_str

--- Parallel_Structure.sh
+++ Parallel_Structure.sh
@@ -6,6 +6,7 @@
 #need to change extraparams file "#define RANDOMIZE" =0 and remove # from in front of "#define SEED"
 
 NAME=Testdata1			#supply file name suffix
+MINK=1					#supply minimum K value (usually 1)
 MAXK=5					#supply max number of K value to test (K = 1-? )
 NREP=10					#supply number of reps for each Kvalue test
 
@@ -16,11 +17,11 @@ if [ -f ./commands.txt ]
 then rm commands.txt
 fi
 
-COUNTER=1 
+COUNTER=$MINK 
 while [  $COUNTER -le $MAXK  ]; do 
 	REP=1
 	while [ $REP -le $NREP ]; do
-		echo "nohup ./structure -D $[RANDOM % 999999999] -K $COUNTER -o ./results/"$NAME"_K"$COUNTER"_rep"$REP" 2> ./nohup.out < /dev/null " >> commands.txt
+		echo "nohup ./structure -D $[RANDOM % 999999999] -K $COUNTER -o ./results/"$NAME"_K"$COUNTER"_rep"$REP" 2>&1 " >> commands.txt
 		REP=$((REP+1))
 		done
 	let COUNTER=COUNTER+1
